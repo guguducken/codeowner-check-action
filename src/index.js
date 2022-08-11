2c8504dca083a6eccc54abbed58f4ead9226ab23
guguducken <1173781832@qq.com>
2022-08-11 16:17:13 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const github_token = core.getInput("github-token", { required: true });
+
+async function run() {
+    try {
+        //get a octokit client
+        const octokit = github.getOctokit(github_token, { userAgent: "guguducken/label-size-action" });
+
+        //get github context
+        const context = github.context;
+
+        //get pull request num
+        const num = context.payload?.pull_request?.number;
+
+        const { files } = await octokit.rest.pulls.listFiles(
+            {
+                ...context.repo,
+                pull_number: num,
+            }
+        );
+        core.info(JSON.stringify(files));
+    } catch (error) {
+        core.setFailed(error.msg);
+    }
+}
